3b3572e068d2b67b4bcb207d6cf9d809b140e593
usb777 <dzsamoila@gmail.com>
2020-03-29 02:43:52 -0400

    Delete User By admin

---
+++ goalproject2/src/Remote/UserDeleteById.ts
@@ -0,0 +1,25 @@
+import { Users } from "../Components/Models/Users";
+import { TokenExpiredError } from "../Components/Errors/TokenExpiredError";
+import { UserNotFoundError } from "../Components/Errors/UserNotFoundError";
+import { InternalServiceError } from "../Components/Errors/InternalServiceError";
+import { Project2Client } from "./Project2Client";
+
+export async function deleteUserById(userId:number|undefined):Promise<Users>{    
+    try{
+        let res = await Project2Client.delete(`/users/${userId}`)
+
+        if(res.status === 400){
+            throw new TokenExpiredError()
+        }
+        return res.data
+    } catch (e) {
+        if(e.status === 400){
+            throw e
+        } else if(e.status === 404){
+            throw new UserNotFoundError()
+        }
+        else{
+            throw new InternalServiceError()
+        }
+    }
+}//end of class
